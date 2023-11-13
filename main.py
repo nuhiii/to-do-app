0166d5ffcf0b742792902aeb2641b841f3d0a02f
Nuhirath Rafthia <nr2793@columbia.edu>
2023-11-13 14:46:50 -0500

    remove match case statements and instead utilize if, elif, else statements to shortcut the way we interact with manipulating data on the list

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = input("Type 'add', 'edit', or 'complete' followed by the todo action, "
+                        "or type 'show' to display list or 'exit' to quit program: ").strip()
 
-    match user_action.strip():
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
+    if "add" in user_action:
+        todo = user_action[4:] + "\n"
 
-            # opens existing data from file to preserve
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        # opens existing data from file to preserve
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            # append new items
-            todos.append(todo)
+        # append new items
+        todos.append(todo)
 
-            # overwrites with a new file the list data
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
-        case "show" | "display":
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        # overwrites with a new file the list data
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-            # # list comprehension to remove the extra \n line that print generates on top of existing \n on list
-            # # when printing to console
-            # new_todos = [item.strip("\n") for item in todos]
+    elif "show" in user_action:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                item = item.strip("\n")
-                print(f"{index + 1}. {item}")
-        case "edit":
-            number = int(input("Number of the todo to edit: "))
-            todo = input("Enter new todo: ") + "\n"
+        # # list comprehension to remove the extra \n line that print generates on top of existing \n on list
+        # # when printing to console
+        # new_todos = [item.strip("\n") for item in todos]
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip("\n")
+            print(f"{index + 1}. {item}")
 
-            todos[number - 1] = todo
+    elif "edit" in user_action:
+        number = int(user_action[5:])
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        todos[number - 1] = input("Enter new todo: ") + "\n"
 
-            completed = todos.pop(number - 1).strip("\n")
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+    elif "complete" in user_action:
+        number = int(user_action[9:])
 
-            print(f'The to do item "{completed}" was removed from the list.')
-        case "exit":
-            break
-        case _:
-            print("You entered an unknown command")
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
+        completed = todos.pop(number - 1).strip("\n")
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+
+        print(f'The to do item "{completed}" was removed from the list.')
+
+    elif "exit" in user_action:
+        break
+
+    else:
+        print("You entered an unknown command")
 
 print("Bye!")
