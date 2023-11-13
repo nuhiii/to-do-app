2836eb2f70088715567e5ab8dfa0c2538d239ae5
Nuhirath Rafthia <nr2793@columbia.edu>
2023-11-13 18:06:56 -0500

    package our functions in a separate file and import the script as a module to our main code

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of
-    to-do items.
-    """
-    # opens existing data from file to preserve
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """
-    Write the to-do items list in the text file.
-    """
-    # overwrites with a new file the list data
-    with open(filepath, "w") as file_local:
-        file_local.writelines(todos_arg)
-
+# from functions import get_todos, write_todos
+# with the above code we don't have to write functions.method()
+# as we are importing the specific functions we will use
+# if we had a folder named modules where functions.py existed, then we write:
+# from modules import functions
+# with the below, we have to use functions.method() but it makes it easier
+# to see where our functions are coming from
+import functions
 
 while True:
     user_action = input("Type 'add', 'edit', or 'complete' followed by the todo action, "
@@ -24,13 +13,13 @@ while True:
 
     if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
-        todos = get_todos()
+        todos = functions.get_todos()
         # append new items
         todos.append(todo)
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = functions.get_todos()
         for index, item in enumerate(todos):
             item = item.strip("\n")
             print(f"{index + 1}. {item}")
@@ -38,9 +27,9 @@ while True:
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
-            todos = get_todos()
+            todos = functions.get_todos()
             todos[number - 1] = input("Enter new todo: ") + "\n"
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -48,9 +37,9 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = functions.get_todos()
             completed = todos.pop(number - 1).strip("\n")
-            write_todos(todos)
+            functions.write_todos(todos)
             print(f'The to do item "{completed}" was removed from the list.')
         except IndexError:
             print("There is no item with that number.")
