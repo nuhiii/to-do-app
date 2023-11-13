63de60732460e7525c9f43d5f05806f27a2f05bb
Nuhirath Rafthia <nr2793@columbia.edu>
2023-11-13 16:48:06 -0500

    include a function to write data to file and restructure remaining code

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def get_todos():
+def get_todos(filepath):
     # opens existing data from file to preserve
-    with open("todos.txt", "r") as file_local:
+    with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
+
+def write_todos(filepath, todos_arg):
+    # overwrites with a new file the list data
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
 
 
 while True:
@@ -11,19 +17,13 @@ while True:
 
     if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
-
-        todos = get_todos()
-
+        todos = get_todos("todos.txt")
         # append new items
         todos.append(todo)
-
-        # overwrites with a new file the list data
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()
-
+        todos = get_todos("todos.txt")
         for index, item in enumerate(todos):
             item = item.strip("\n")
             print(f"{index + 1}. {item}")
@@ -31,13 +31,9 @@ while True:
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
-
-            todos = get_todos()
-
+            todos = get_todos("todos.txt")
             todos[number - 1] = input("Enter new todo: ") + "\n"
-
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -45,14 +41,9 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-
-            todos = get_todos()
-
+            todos = get_todos("todos.txt")
             completed = todos.pop(number - 1).strip("\n")
-
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
-
+            write_todos("todos.txt", todos)
             print(f'The to do item "{completed}" was removed from the list.')
         except IndexError:
             print("There is no item with that number.")
