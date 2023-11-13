16064a93b77be4e7f663598b6bcb8517ffa5cf75
Nuhirath Rafthia <nr2793@columbia.edu>
2023-11-13 09:54:11 -0500

    provide complete feature to take item off list

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
 
     match user_action.strip():
         case "add":
@@ -10,11 +10,14 @@ while True:
         case "show" | "display":
             for index, item in enumerate(todos):
                 item = item.title()
-                print(f"{index}. {item}")
+                print(f"{index + 1}. {item}")
         case "edit":
             number = int(input("Number of the todo to edit: "))
             todo = input("Enter new todo: ")
             todos[number - 1] = todo
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
         case "exit":
             break
         case _:
