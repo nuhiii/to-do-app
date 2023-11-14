8daf26ec8b44a290f11b1e9ef53bfd126b470793
Nuhirath Rafthia <nr2793@columbia.edu>
2023-11-14 17:13:43 -0500

    incorporate input boxes and add button for to do list in the gui window app

--- gui.py
+++ gui.py
@@ -1,8 +1,14 @@
 import functions
 import PySimpleGUI
 
+# create a text type of the gui instance to create a label on the window
+label = PySimpleGUI.Text("Type in a to-do")
+input_box = PySimpleGUI.InputText(tooltip="Enter todo")
+add_button = PySimpleGUI.Button("Add")
+
 # create an instance of a window type
-window = PySimpleGUI.Window("My To-Do App", layout=[""])  # title of the window
+window = PySimpleGUI.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+# title of the window and each row [] displayed
 
 # display the window on the screen - like print func
 window.read()
