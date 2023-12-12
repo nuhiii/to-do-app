e8b918aecb149e7578384af9c0fbb61106ba13c1
Nuhirath Rafthia <nr2793@columbia.edu>
2023-12-12 16:15:15 -0500

    set up interface design with title, checkboxes generated from todo list function given txt file, and text input box, all without any functionality yet

--- web.py
+++ web.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import functions
 
+todos = functions.get_todos()
+
+# title
 st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+# checkboxes
+for todo in todos:
+    st.checkbox(todo)
+
+# input
+st.text_input(label="", placeholder="Add new todo...")
